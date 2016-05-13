51bf1c17cc7910f6cfde103a7a4363e2e7341bfe
darshilpatel <darshil-patel@hotmail.com>
2016-05-12 23:42:04 -0400

    update

--- Variables, Datatypes and Operators/ByteShortInt_SimpleChallange.java
+++ Variables, Datatypes and Operators/ByteShortInt_SimpleChallange.java
@@ -5,6 +5,14 @@ public class ByteShortInt_SimpleChallange {
 
     public static void main (String [] args){
 
-        
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 5000L + 10L *  (byteValue+shortValue+intValue);
+        short shortTotal = (short) (100 + 10*(byteValue+shortValue+intValue));
+        System.out.println(longTotal);
+        System.out.println(shortTotal);
+
     }
 }
